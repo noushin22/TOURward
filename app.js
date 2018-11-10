4ba873d2cae0340b99f93a4b30b2a73541f2aeca
Sadia Hasan <sadiazh729@gmail.com>
2018-11-10 13:30:05 -0500

    jhjh

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ app.get('/', (req, res) => {
 	res.render('home.hbs');
 });
 
+app.get('/profile', (req, res) => {
+	res.render('profile.hbs');
+});
+
 app.listen(process.env.PORT || 8000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
